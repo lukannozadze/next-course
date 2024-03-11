91b7bd078d39f39c632d5f8e83e4b01267b91d7b
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-11 18:53:39 +0400

    error page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -3,11 +3,19 @@ import { notFound } from "next/navigation";
 type Params = {
   params: { productId: string; reviewId: string };
 };
-
+function getRandomInt(count:number){
+  return Math.floor(Math.random() * count);
+}
 export default function ReviewDetail({ params }: Params) {
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
+
+ const random = getRandomInt(2);
+ if(random === 1){
+  throw new Error('Error loading review');
+ }
+
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
