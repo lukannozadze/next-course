f2d9f456437341c48b04393ad9ad67fd0d009e45
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-11 19:04:58 +0400

    recovering from errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,9 @@
 'use client'
-export default function ErrorBoundary({error}:{error:Error}) {
+export default function ErrorBoundary({error,reset}:{error:Error; reset:()=>void}) {
   return (
+    <>
     <div>Error in reviewId {error.message}</div>
+    <button onClick={()=>{reset}}>Try Again</button>
+    </>
   )
 }
