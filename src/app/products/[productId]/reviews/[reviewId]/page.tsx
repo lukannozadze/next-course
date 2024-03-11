4db9b3b2f50011b43da92535bcb8d56b7be4de27
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-11 13:12:54 +0400

    not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 type Params = {
   params: { productId: string; reviewId: string };
 };
 
 export default function ReviewDetail({ params }: Params) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
