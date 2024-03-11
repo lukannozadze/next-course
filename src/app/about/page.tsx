456c82bcf4fe503c7da9bbc8fe07350bf6a41cc7
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-11 16:04:56 +0400

    routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,7 @@
 export default function Profile() {
-  return (
-    <div>My Profile!</div>
-  )
+  return <div>My Profile!</div>;
 }
+
+export const metadata = {
+  title: "About Codevolution",
+};
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,21 @@
+import { Metadata } from "next";
+
+export const generateMetadata = async ({
+  params,
+}: Params): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`);
+    }, 500);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
 type Params = {
-  params: { productId: string }
+  params: { productId: string };
 };
 
-export default function ProductDetails({params}:Params) {
+export default function ProductDetails({ params }: Params) {
   return <h1>Details About product {params.productId}</h1>;
 }
