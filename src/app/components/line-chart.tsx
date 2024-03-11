4a9726e0b9ad2800052f2b6ad2c9d9095ac77657
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-11 13:55:50 +0400

    file collocation

---
+++ src/app/components/line-chart.tsx
@@ -0,0 +1,6 @@
+
+export default function LineChart() {
+  return (
+    <h2>Line Chart</h2>
+  )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import LineChart from "../components/line-chart";
+
+export default function page() {
+  return (
+    <>
+      <h1>Bar Chart</h1>
+      <LineChart />
+    </>
+  );
+}
