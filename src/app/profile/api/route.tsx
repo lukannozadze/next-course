650dce0470e76a0608d55d6740a10e52ab1a0284
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-14 12:45:26 +0400

    cookies in route handler

--- src/app/profile/api/route.tsx
+++ src/app/profile/api/route.tsx
@@ -1,14 +1,15 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers,cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const headerList = headers();
-
+  
+  cookies().set('resultsperpage','20');
   console.log(headerList.get("Authorization"));
   console.log(requestHeaders.get("Authorization"));
-  
+  console.log(cookies().get('resultsperpage'))
   return new Response("<h1>Profile Api Data</h1>", {
-    headers: { "Content-Type": "text/html" },
+    headers: { "Content-Type": "text/html", "Set-Cookie": "theme=dark" },
   });
 }
